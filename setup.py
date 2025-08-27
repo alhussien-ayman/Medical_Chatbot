68c257406072ca6abc8e47795e027219b7626cfa
alhussien-ayman <alhussienaymanhanafycourses@gmail.com>
2025-08-27 15:43:44 +0200

    add folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages , setup 
+
+setup (
+    name = 'Generative AI project' ,
+    version = '0.0.0',
+    auther = 'Alhussien Ayman',
+    packages=find_packages(),
+    install_requires =[]
+)
